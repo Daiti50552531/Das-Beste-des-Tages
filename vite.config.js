c39aea6812e64e255e377241a84749ddbf001c3c
Daiti50552531 <davidleinders@web.de>
2025-06-27 11:03:14 +0200

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+javascriptimport { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Das-Beste-des-Tages/',
+})
